e66ecaed4ddd45b54c662c5f2d6d4a257b74ba9a
ShivamBhalodia <71123203+ShivamBhalodia@users.noreply.github.com>
2020-09-15 23:17:29 +0530

    FinalCommit

--- ProrityQueue/MaxHeap.h
+++ ProrityQueue/MaxHeap.h
@@ -1,115 +1,112 @@
-///author : Kartik Arora
+// author : ShivamBhalodia
 
-#ifndef MAXHEAP_H
-#define MAXHEAP_H
-
+#ifndef MAXHEAP_H  //if MaxHeap.h hasn't been included yet
+#define MAXHEAP_H  //#define it so the compiler knows it has been included and can prevent including it twice
 #include<vector>
+#define left_child(i) (2*i+1)
+#define right_child(i) (2*i+2)
+#define parent_node(i) ((i-1)/2)
 
 template<class T>
 class MaxHeap
 {
     public:
-        /*
-         *constructs an empty Max heap
-         *this max heap will hold objects of type T
-         *the priority of the objects will be decided
-         *by a user defined function.
-        */
-        MaxHeap<T> (bool (*comp)(T obj1, T obj2));
-        //return true if the Max Heap is empty, true otherwise.
-        bool isEmpty();
-        //used to insert an item in the priority queue.
-        void push(T obj);
-        //deletes the highest priority item currently in the queue.
-        void pop();
-        //return the highest priority item currently in the queue.
+        
+        //Construct empty heap of user defined priority
+        MaxHeap<T>(bool (*comp)(T a,T b));
+        //return the highest priority item currently in the Max Heap.
         T top();
+        //Insert an item in the Max Heap.
+        void push(T a);
+        //deletes the highest priority item currently in the Max Heap.
+        void pop();
+        //To check whether the Max Heap is empty or not
+        bool isEmpty();
+        
     private:
+      
         //return true if obj1 compares less than obj2
-        bool (*compare)(T obj1, T obj2);
-        //the max heap is implemented using a vector.
-        std::vector<T> heap;
-        void heapifyUp(int idx);
-        void heapifyDown(int idx);
-        int leftChild(int i)
-        {
-            return 2*i + 1;
-        }
-        int rightChild(int i)
-        {
-            return 2*i + 2;
-        }
-        int parent(int i)
-        {
-            return (i-1)/2;
-        }
+        bool (*compare)(T a,T b);
+        std::vector<T>heap;
+        void heapifyUpward(int idx);
+        void heapifyDownward(int idx);
+        
 };
-template<class T> MaxHeap<T>::MaxHeap(bool (*comp)(T obj1, T obj2))
+
+template<class T> 
+MaxHeap<T>::MaxHeap(bool (*comp)(T a,T b))
 {
-    this -> compare = comp;
+    this->compare=comp;
 }
 
-template<class T> bool MaxHeap<T>::isEmpty()
+template<class T> 
+T MaxHeap<T>::top()
+{
+    if(!isEmpty())
+    {
+      return heap[0];
+    }
+}
+
+template<class T> 
+void MaxHeap<T>::push(T a)
+{
+    heap.push_back(a);             //First insert new element at the end of the Max heap
+    heapifyUpward(heap.size()-1);    //Then if Max heap is disturbed then fix it
+}
+
+template<class T> 
+void MaxHeap<T>::pop()
+{
+    if(!isEmpty())
+    {
+      	std::swap(heap[0],heap[heap.size()-1]);           ////First replace first element with the last element
+        heap.pop_back();
+        if(!isEmpty()) 
+       	heapifyDownward(0);            //Then if Max heap is disturbed then fix it
+    }
+}
+
+template<class T> 
+bool MaxHeap<T>::isEmpty()
 {
     return heap.empty();
 }
 
-template<class T> void MaxHeap<T>::push(T obj)
+template<class T> 
+void MaxHeap<T>::heapifyUpward(int idx)           //Fix Max heap in upward direction
 {
-    heap.push_back(obj);
-    heapifyUp(heap.size()-1);
-}
-
-template<class T> void MaxHeap<T>::pop()
-{
-    if(!isEmpty())
+    int p=parent_node(idx);
+    if(p<0)
+        return;
+    if(compare(heap[p],heap[idx]))
     {
-       std::swap(heap[0], heap[heap.size()-1]);
-       heap.pop_back();
-       if(!isEmpty())
-          heapifyDown(0);
+        std::swap(heap[p],heap[idx]);
+        heapifyUpward(p);
     }
 }
 
-template<class T> T MaxHeap<T>::top()
+template<class T> 
+void MaxHeap<T>::heapifyDownward(int idx)         //Fix Max heap in downward direction
 {
-    if(!isEmpty())
+    int largeIdx=idx;
+    int l=left_child(idx);
+    int r=right_child(idx);
+    if(l<heap.size())
     {
-       return heap[0];
+        if(compare(heap[largeIdx],heap[l]))
+            largeIdx=l;
     }
-}
-
-template<class T> void MaxHeap<T>::heapifyDown(int idx)
-{
-    int largeIdx = idx;
-    int leftChildIdx = leftChild(idx), rightChildIdx = rightChild(idx);
-    if(leftChildIdx < heap.size())
+    if(r<heap.size())
     {
-       if(compare(heap[largeIdx], heap[leftChildIdx]))
-            largeIdx = leftChildIdx;
-    }
-    if(rightChildIdx < heap.size())
-    {
-       if(compare(heap[largeIdx], heap[rightChildIdx]))
-            largeIdx = rightChildIdx;
+        if(compare(heap[largeIdx],heap[r]))
+            largeIdx=r;
     }
 
-    if(largeIdx != idx)
+    if(largeIdx!=idx)
     {
-       std::swap(heap[largeIdx], heap[idx]);
-       heapifyDown(largeIdx);
-    }
-}
-
-template<class T> void MaxHeap<T>::heapifyUp(int idx)
-{
-    int parentIdx = parent(idx);
-    if(parentIdx < 0)
-            return;
-    if(compare(heap[parentIdx], heap[idx]))
-    {
-        std::swap(heap[parentIdx], heap[idx]);
-        heapifyUp(parentIdx);
+       std::swap(heap[largeIdx],heap[idx]);
+       heapifyDownward(largeIdx);
     }
 }
 
