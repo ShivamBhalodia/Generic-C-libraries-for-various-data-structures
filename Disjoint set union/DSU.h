0afd21ef2e8abb8fc6810c6772e5da36d68437e3
ShivamBhalodia <71123203+ShivamBhalodia@users.noreply.github.com>
2020-09-18 22:55:30 +0530

    Update DSU.h

--- Disjoint set union/DSU.h
+++ Disjoint set union/DSU.h
@@ -1,4 +1,5 @@
 //author : ShivamBhalodia
+
 #ifndef DSU_H   //if DSU.h hasn't been included yet
 #define DSU_H //#define it so the compiler knows it has been included and can prevent including it twice
 #include <vector>
@@ -6,29 +7,34 @@
 class DSU
 {
     public:
+      
         //constructor that creats n different groups each with 1 element itself
         DSU(int n);
         
-        //add a new group
-        void newGroup();
-
-    	//finds the parent of the group to which element belongs
-        int Find(int a);
+        //find the parent of the group to which element belongs
+        int findParent(int a);
         
         //returns the number of elements in the group to which element belongs
         int groupSize(int a);
 
         //return true if element refers to a valid group
-        bool isValidId(int a);
+        bool isValidGroup(int a);
+        
+        //Check whether element a and element b belongs to same group or not
+        bool inSameGroup(int a,int b);
         
         //merge two groups to which element a and element b belong
         void Union(int a,int b);
+        
+        //add a new group
+        void newGroup();
 
         //returns number of groups
         int getnoOfGroups()
         {
             return noOfGroups;
         }
+        
     private:
         int noOfGroups;
         std::vector<int>parents;
@@ -37,35 +43,20 @@ class DSU
 
 DSU::DSU(int n)
 {
-  	noOfGroups=n;
     parents=std::vector<int>(n);
     sizee=std::vector<int>(n,1);   //initially size is 1
     for(int i=0;i<n;i++)
         parents[i]=i;    //initially make element to parent of itself
+    noOfGroups=n;
 }
 
-void DSU::newGroup()
+int DSU::findParent(int a)
 {
-   noOfGroups++;
-   int a=parents.size();
-   parents.push_back(a);
-   sizee.push_back(1);
-}
-
-bool DSU::isValidId(int a)
-{
-    if(a>=0 && a<parents.size())
-        return 1;
-    return 0;
-}
-
-int DSU::Find(int a)
-{
-    if(!isValidId(a))
-      	return -1;
+    if(!isValidGroup(a))
+        return -1;
     
     while(parents[a]!=a)
-  	{
+    {
         parents[a]=parents[parents[a]];
         a=parents[a];
     }
@@ -75,31 +66,57 @@ int DSU::Find(int a)
 
 int DSU::groupSize(int a)
 {
-    if(!isValidId(a))
-    	return -1;
-    return sizee[Find(a)];
+    if(!isValidGroup(a))
+      	return -1;
+    return sizee[findParent(a)];
+}
+
+bool DSU::isValidGroup(int a)
+{
+    if(a>=0 && a<parents.size())
+        return true;
+    return false;
+}
+
+bool DSU::inSameGroup(int a,int b)
+{
+    if(findParent(a)==findParent(b)) 
+      	return true;
+    return false;
 }
 
 void DSU::Union(int a,int b)
 {
-    if((!isValidId(a)) || (!isValidId(b)))
+    if((!isValidGroup(a)) || (!isValidGroup(b)))
         return;
-    int group1=Find(a);
-    int group2=Find(b);
-    if(group1==group2)
+        
+    if(inSameGroup(a,b))
         return;
+        
+    int parent_a=findParent(a);
+    int parent_b=findParent(b);
+    
     noOfGroups--;
     //append smaller group to bigger group
-    if(sizee[group1]>=sizee[group2])     
+    if(sizee[parent_a]>=sizee[parent_b])     
     {
-        parents[group2]=group1;
-        sizee[group1]+=sizee[group2];
+        parents[parent_b]=parent_a;
+        sizee[parent_a]+=sizee[parent_b];
     }
     else
     {
-        parents[group1]=group2;
-        sizee[group2]+=sizee[group1];
+        parents[parent_a]=parent_b;
+        sizee[parent_b]+=sizee[parent_a];
     }
 }
+
+void DSU::newGroup()
+{
+	int a=parents.size();
+	parents.push_back(a);
+	sizee.push_back(1);
+	noOfGroups++;
+}
+
 
 #endif //DSU_H
